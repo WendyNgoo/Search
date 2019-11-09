e9cbe3d0d7a176044492a1df788aa4db101851ed
kheeong <42634274+kheeong@users.noreply.github.com>
2019-11-10 00:50:52 +0800

    hi

--- app/src/main/java/com/example/search/MainActivity.java
+++ app/src/main/java/com/example/search/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,34 +18,23 @@ import java.util.Arrays;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-ListView listview;
+    ListView listview;
     ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listview = (ListView)findViewById(R.id.listview);
-        ArrayList<String> values = new ArrayList<>();
-        values.addAll(Arrays.asList(getResources().getStringArray(R.array.rooms)));
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>((this),android.R.layout.simple_list_item_1,android.R.id.text1,values);
-listview.setAdapter(adapter);
-listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        if (position == 0){
-            Intent intent = new Intent(view.getContext(),Main2Activity.class);
-            startActivityForResult(intent,0);
-        }
-        if (position == 1){
-            Intent intent = new Intent(view.getContext(),Main3Activity.class);
-            startActivityForResult(intent,1);
-        }
-        if (position == 2){
-            Intent intent = new Intent(view.getContext(),Main2Activity.class);
-            startActivityForResult(intent,2);
-        }
-    }
-});
+        ArrayList<String> arrayFood = new ArrayList<>();
+        arrayFood.addAll(Arrays.asList(getResources().getStringArray(R.array.rooms)));
+        listview.setAdapter(adapter);
+        adapter = new ArrayAdapter<String>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                arrayFood
+        );
+
+        listview.setAdapter(adapter);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -52,6 +42,25 @@ listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
         inflater.inflate(R.menu.search, menu);
         MenuItem item = menu.findItem(R.id.search_m);
         SearchView searchView = (SearchView) item.getActionView();
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view,int position, long l) {
+                String i = String.valueOf(adapter.getItem(position));
+                Toast.makeText(MainActivity.this,i, Toast.LENGTH_SHORT).show();
+                if (position == 0){
+                    Intent intent = new Intent(view.getContext(),Main2Activity.class);
+                    startActivityForResult(intent,0);
+                }
+                if (position == 1){
+                    Intent intent = new Intent(view.getContext(),Main3Activity.class);
+                    startActivityForResult(intent,1);
+                }
+                if (position == 2){
+                    Intent intent = new Intent(view.getContext(),Main2Activity.class);
+                    startActivityForResult(intent,2);
+                }
+            }
+        });
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
